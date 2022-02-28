e11a97c3e0bfdb6dfc9a52ee652bc06d8adbef9c
Esakor <100299697+Esakor@users.noreply.github.com>
2022-02-28 11:13:24 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let number = 0
+basic.showLeds(`
+    . . # # .
+    . # . . .
+    . . # # .
+    . . . # .
+    . # # . .
+    `)
+let OUT = 0
+if (number == 3) {
+    number = OUT
+}
+basic.forever(function () {
+    if (true) {
+    	
+    } else {
+    	
+    }
+})
